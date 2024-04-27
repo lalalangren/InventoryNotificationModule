f6dde4d19958ac90ad8f7e7531c02250a29bade1
6ZLeo <136341102+6ZLeo@users.noreply.github.com>
2024-04-27 20:11:55 +0800

    曾令腾commit

--- Front-end/Notification.vue
+++ Front-end/Notification.vue
@@ -24,7 +24,53 @@
   </template>
   
   <script>
-  
+  import axios from '@/axios/axios';
+  export default {
+    name: 'Notification',
+    data() {
+      return {
+        notifications: []
+      };
+    },
+    created() {
+      // 在组件创建时检查库存并生成通知
+      this.checkInventory();
+      this.showPermissionsLink = userRole === 'admin';
+    },
+    methods: {
+      async checkInventory() {
+        try {
+          const token = sessionStorage.getItem("access_token");
+          const config = { headers: { 'Authorization': token } };
+          const response = await axios.get('/products/all', config);
+          const inventory = response.data;
+          
+          // 检查库存，如果低于阈值则生成通知
+          inventory.forEach(product => {
+            const leaststock = 1700;
+            if (product.stockQuantity < leaststock) {
+              const notification = {
+                message: `部件${product.productName}库存低于阈值${leaststock}`,
+                productId: product.productID
+              };
+              this.notifications.push(notification);
+            }
+          });
+          console.log(inventory);
+          console.log(this.notifications);
+        } catch (error) {
+          console.error('Error checking inventory:', error);
+        }
+      },
+      removeNotification(index) {
+        this.notifications.splice(index, 1);
+      },
+      goToInventory() {
+        
+        this.$router.push('/inventory-management');
+      }
+    }
+  };
   </script>
   
   <style scoped>
